8f9b4848c47ee05e8e8180a507034c0810ccb8b7
Oskar Villa <100952154+OshkarVTec@users.noreply.github.com>
2022-11-29 19:38:13 -0600

    Publish db to thingspeak

--- dba.ino
+++ dba.ino
@@ -1,9 +1,39 @@
 #include <driver/i2s.h>
 #include "simpleDSP.h"
 #include "FFT.h"
+#include <esp_wpa2.h>
+#include "ThingSpeak.h"
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
 
+WiFiClient  client;
+
+// Insert your network credentials
+const char* ssid = "Tec"; 
+#define EAP_IDENTITY "a01275287@tec.mx"
+#define EAP_PASSWORD "Oskar310802$"
+
+// Writing credentials
+unsigned long myChannelNumber = 1;
+const char * myWriteAPIKey = "5IVATAVA6XG7PI2I";
+
+// Reading credentials
+unsigned long StationChannelNumber = 1950128;
+unsigned long FieldNumber = 1;
+const char * myReadAPIKey = "7JN3HEU1DRXH7BJK";
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+
+#define DB_LIMIT 70
 #define GAIN 20 //Gain for the dB's calibration
-#define SAMPLE_BUFFER_SIZE 4096 //Fs*0.5 seg, closest 2 power 2^12
+
+//MIC SETTINGS
+#define SAMPLE_BUFFER_SIZE 2048 //Fs*0.5 seg, closest 2 power 
 #define SAMPLE_RATE 8000
 
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
@@ -73,6 +103,31 @@ void setup()
     // Start up the I2S peripheral
     i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
     i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
+
+     // WPA2 enterprise magic starts here
+    WiFi.disconnect(true);      
+    WiFi.mode(WIFI_STA);   //init wifi mode
+    Serial.printf("Connecting to WiFi: %s ", ssid);
+    // esp_wifi_sta_wpa2_ent_set_ca_cert((uint8_t *)incommon_ca, strlen(incommon_ca) + 1);
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+    esp_wpa2_config_t configW = WPA2_CONFIG_INIT_DEFAULT();
+    esp_wifi_sta_wpa2_ent_enable(&configW);
+    // WPA2 enterprise magic ends here
+    WiFi.begin(ssid);
+  
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();  
+  
+    ThingSpeak.begin(client);  // Initialize ThingSpeak
     
     //Filter constructor
     iirInit(&iir1, 7, coefB, 7, coefA);
@@ -129,8 +184,54 @@ void loop()
     }
     sum = sum / 1;
     float p = ((float)1/(SAMPLE_BUFFER_SIZE/2))*sum;
-    float dba = 10*log(p)+GAIN;
-    Serial.println(dba);
+    float db = 10*log(p)+GAIN;
+    Serial.println(db);
+
+    
+    if ((millis() - lastTime) > timerDelay) 
+    {
+    
+        // Connect or reconnect to WiFi
+        if(WiFi.status() != WL_CONNECTED)
+        {
+            Serial.print("Attempting to connect");
+            while(WiFi.status() != WL_CONNECTED)
+            {
+                WiFi.begin(ssid); 
+                delay(5000);     
+            } 
+            Serial.println("\nConnected.");
+        }
+        if (db > DB_LIMIT){
+            // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+            // pieces of information in a channel. .
+            int x = ThingSpeak.writeField(myChannelNumber, 2, db, myWriteAPIKey);
+    
+            if(x == 200)
+            {
+                Serial.print("Channel update successful. ");
+                Serial.println(db);
+            }
+            else
+            {
+                Serial.println("Problem updating channel. HTTP error code " + String(x));
+            }
+            lastTime = millis();
+        }    
+         // Read from ThingSpeak
+  
+//        float y = ThingSpeak.readFloatField(StationChannelNumber, FieldNumber, myReadAPIKey);
+//        int statusCode = ThingSpeak.getLastReadStatus();
+//
+//        if(statusCode == 200)
+//        {
+//            Serial.println(String(y));
+//        }
+//        else
+//        {
+//            Serial.println("Problem reading channel");
+//        }
+    }
     fft_destroy(real_fft_plan);
-    delay(1000);
+    
 }
